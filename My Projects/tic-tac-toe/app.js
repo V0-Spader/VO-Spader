3aa797c8641f524f2b3a16ad14e703332c23805a
Vo Spader <Travisschaak@Gmail.com>
2023-08-02 18:21:14 -0400

    added event listerner to the game board

--- My Projects/tic-tac-toe/app.js
+++ My Projects/tic-tac-toe/app.js
@@ -29,7 +29,7 @@ form.addEventListener('submit', (event) => {
 
 const initializeVar = (data) => {
     data.choice = +data.choice;
-    data.board = [0,1,2,3,4,5,6,7,8];
+    data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     data.player1 = "X";
     data.player2 = "O";
     data.round = 0;
@@ -37,12 +37,39 @@ const initializeVar = (data) => {
     data.gameOver = false;
 }
 
+const playMove = (box, data) => {
+//is game over? If game over, don't do anything
+if (data.gameOver || data.round > 8) {
+    return
+}
+//check if game box has a letter in it, if so then don't do anything
+if(data.board[box.id] === "X" || data.board[box.id] === "O") {
+    return;
+}
+
+//adjust the DOM for fplayer move, and then check win consition
+data.board[box, id] = data.currentPlayer;
+box.textContent = data.currentPlayer;
+box.classList.add(data.currentPlayer === "X" ? "player1" : "player2");
+//increse the round #
+data.round++;
+
+//check end conditions
+};
+
+const addEventListenerToGameBoard = (data) => {
+    document.querySelectorAll('.box').forEach(box => {
+        box.addEventListener('click', (event) => {
+            playMove(event.target, data)
+        })
+    })
+}
+
 const initializeGame = (data) => {
     //initialize game variable
     initializeVar(data);
-
-
+    
     //add event listener to the gameboard
-
+    addEventListenerToGameBoard(data);
 
 }
